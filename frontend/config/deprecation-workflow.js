69995aa67997e2aa2d8a481735ddb273ae15aaf2
Mitan Omar <mitan@gmx.ch>
2024-07-23 13:44:06 +0200

    chore(frontend): new deprecation to prepare fixing it before next ember upgrade

--- frontend/config/deprecation-workflow.js
+++ frontend/config/deprecation-workflow.js
@@ -4,5 +4,15 @@ self.deprecationWorkflow.config = {
   workflow: [
     { handler: "silence", matchId: "ember-string.add-package" }, // https://deprecations.emberjs.com/id/ember-string-add-package
     { handler: "silence", matchId: "ensure-safe-component.string" }, // optimized-power-select
+    { handler: "silence", matchId: "ember-data:deprecate-array-like" },
+    { handler: "silence", matchId: "ember-data:deprecate-promise-proxies" },
+    {
+      handler: "silence",
+      matchId: "ember-data:deprecate-promise-many-array-behaviors",
+    },
+    {
+      handler: "silence",
+      matchId: "ember-data:deprecate-non-strict-relationships",
+    },
   ],
 };
